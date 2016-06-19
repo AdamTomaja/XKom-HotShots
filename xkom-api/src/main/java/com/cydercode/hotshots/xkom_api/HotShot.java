304dae66d3edace78ecf1249b37ab043527f310c
cydercode_adam <cydercode@gmail.com>
2016-06-19 12:45:29 +0200

    xkom-api project created

---
+++ xkom-api/src/main/java/com/cydercode/hotshots/xkom_api/HotShot.java
@@ -0,0 +1,14 @@
+package com.cydercode.hotshots.xkom_api;
+
+import java.math.BigDecimal;
+
+public class HotShot {
+	
+	private String productName;
+	private BigDecimal oldPrice;
+	private BigDecimal newPrice;
+	private int discountPercent;
+	
+
+	
+}
